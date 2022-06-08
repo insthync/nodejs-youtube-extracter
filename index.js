ec3894c5f5cc40bca9b3becf5090aa804afe6d5f
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-06-08 11:29:04 +0700

    Try to make a proxy

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const cors = require('cors');
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
+const m3u8stream = require('m3u8stream');
 
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+//app.use('/media', express.static('media'))
 
+const port = Number(process.env.SERVER_PORT || 8000);
+const useHttps = Number(process.env.USE_HTTPS || 0) > 0;
+const keyFilePath = process.env.HTTPS_KEY_FILE_PATH;
+const certFilePath = process.env.HTTPS_CERT_FILE_PATH;
+const httpsPort = Number(process.env.HTTPS_SERVER_PORT || 8080);
 const caches = {};
 
 const Extract = async (id) => {
@@ -23,6 +30,37 @@ const Extract = async (id) => {
         youtubeSkipDashManifest: true
     });
     return resp;
+};
+
+const GetResponseUrl = function (key, req) {
+    const serveUrl = (useHttps ? "https://" : "http://") + req.subdomains.join('.') + req.hostname + ":" + (useHttps ? httpsPort : port);
+    return `${serveUrl}/${key}`;
+}
+
+const Response = function (id, key, extractedData, req, res) {
+    /*
+    var file = fs.createWriteStream(`./media/${key}.m3u8`);
+    https.get(extractedData.url, function (response) {
+        response.pipe(file);
+        file.on('finish', function () {
+            file.close(() => {
+                StoreToCache(key, extractedData);
+                res.status(200).send({
+                    url: GetResponseUrl(key, req),
+                });
+            });
+        });
+    }).on('error', function (err) { // Handle errors
+        fs.unlink(dest); // Delete the file async. (But we don't check the result)
+        res.status(500).send({
+            "message": "Cannot extract YouTube URL from `" + id + "`, File error: `" + err.message + "`",
+        });
+    });
+    */
+    StoreToCache(key, extractedData);
+    res.status(200).send({
+        url: GetResponseUrl(key, req),
+    });
 };
 
 const FormatsCompareAsc = async (a, b) => {
@@ -45,8 +83,7 @@ const StoreToCache = (key, url) => {
 const GetFromCache = (key) => {
     if (key in caches) {
         // If it were cached 10 second ago, re-extract it
-        if (Date.now() - caches[key].time >= 10000)
-        {
+        if (Date.now() - caches[key].time >= 10000) {
             return undefined;
         }
         return caches[key].url;
@@ -60,12 +97,35 @@ app.get('/', (req, res) => {
     });
 })
 
+app.get('/:key', async (req, res) => {
+    const key = req.params.key;
+    const cacheData = GetFromCache(key);
+    if (cacheData === undefined) {
+        res.status(500).send({
+            "message": "Cannot load file with key: `" + key + "`",
+        });
+        return;
+    }
+    console.log(cacheData.url);
+    https.get(cacheData.url, function (response) {
+        res.writeHead(200, { "Content-Type": "application/vnd.apple.mpegurl" });
+        response.pipe(res);
+    }).on('error', function (err) { // Handle errors
+        fs.unlink(dest); // Delete the file async. (But we don't check the result)
+        res.status(500).send({
+            "message": "Cannot extract YouTube URL from `" + id + "`, File error: `" + err.message + "`",
+        });
+    });
+});
+
 app.get('/:id/lowest', async (req, res) => {
     const id = req.params.id;
     const key = id + "_lowest";
     const cacheData = GetFromCache(key);
     if (cacheData !== undefined) {
-        res.status(200).send(cacheData);
+        res.status(200).send({
+            url: GetResponseUrl(key, req),
+        });
     } else {
         const ytResp = await Extract(id);
         const filteredResp = ytResp.formats.sort(FormatsCompareAsc);
@@ -75,8 +135,7 @@ app.get('/:id/lowest', async (req, res) => {
             });
             return;
         }
-        StoreToCache(key, filteredResp[0]);
-        res.status(200).send(filteredResp[0]);
+        Response(id, key, filteredResp[0], req, res);
     }
 });
 
@@ -85,7 +144,9 @@ app.get('/:id/highest', async (req, res) => {
     const key = id + "_highest";
     const cacheData = GetFromCache(key);
     if (cacheData !== undefined) {
-        res.status(200).send(cacheData);
+        res.status(200).send({
+            url: GetResponseUrl(key, req),
+        });
     } else {
         const ytResp = await Extract(id);
         const filteredResp = ytResp.formats.sort(FormatsCompareAsc);
@@ -96,8 +157,7 @@ app.get('/:id/highest', async (req, res) => {
             return;
         }
         filteredResp.reverse();
-        StoreToCache(key, filteredResp[0]);
-        res.status(200).send(filteredResp[0]);
+        Response(id, key, filteredResp[0], req, res);
     }
 });
 
@@ -107,7 +167,9 @@ app.get('/:id/:height', async (req, res) => {
     const key = id + "_" + height;
     const cacheData = GetFromCache(key);
     if (cacheData !== undefined) {
-        res.status(200).send(cacheData);
+        res.status(200).send({
+            url: GetResponseUrl(key, req),
+        });
     } else {
         const ytResp = await Extract(id);
         const filteredResp = ytResp.formats.sort(FormatsCompareAsc);
@@ -124,16 +186,9 @@ app.get('/:id/:height', async (req, res) => {
                 break;
             }
         }
-        StoreToCache(key, filteredResp[indexOfData]);
-        res.status(200).send(filteredResp[indexOfData]);
+        Response(id, key, filteredResp[indexOfData], req, res);
     }
 });
-
-const port = Number(process.env.SERVER_PORT || 8000);
-const useHttps = Number(process.env.USE_HTTPS || 0) > 0;
-const keyFilePath = process.env.HTTPS_KEY_FILE_PATH;
-const certFilePath = process.env.HTTPS_CERT_FILE_PATH;
-const httpsPort = Number(process.env.HTTPS_SERVER_PORT || 8080);
 
 const httpServer = http.createServer(app);
 httpServer.listen(port, () => {
